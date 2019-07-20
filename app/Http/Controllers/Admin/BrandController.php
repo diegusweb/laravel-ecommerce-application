c9fa0020563eed0d2ab272378850000040d5c1ea
Diego Rueda <diego.rueda2@gmail.com>
2019-07-20 10:25:24 -0400

    template brand

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -42,4 +42,74 @@ class BrandController extends BaseController
         $this->setPageTitle('Brands', 'Create Brand');
         return view('admin.brands.create');
     }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $brand = $this->brandRepository->createBrand($params);
+
+        if (!$brand) {
+            return $this->responseRedirectBack('Error occurred while creating brand.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.brands.index', 'Brand added successfully' ,'success',false, false);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $brand = $this->brandRepository->findBrandById($id);
+
+        $this->setPageTitle('Brands', 'Edit Brand : '.$brand->name);
+        return view('admin.brands.edit', compact('brand'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $brand = $this->brandRepository->updateBrand($params);
+
+        if (!$brand) {
+            return $this->responseRedirectBack('Error occurred while updating brand.', 'error', true, true);
+        }
+        return $this->responseRedirectBack('Brand updated successfully' ,'success',false, false);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete($id)
+    {
+        $brand = $this->brandRepository->deleteBrand($id);
+
+        if (!$brand) {
+            return $this->responseRedirectBack('Error occurred while deleting brand.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.brands.index', 'Brand deleted successfully' ,'success',false, false);
+    }
 }
