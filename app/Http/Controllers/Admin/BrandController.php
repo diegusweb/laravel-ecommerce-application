f167f716f5bffcf56fd187ce22d0f866e03ff859
Diego Rueda <diego.rueda2@gmail.com>
2019-07-20 10:09:44 -0400

    brands controller

---
+++ app/Http/Controllers/Admin/BrandController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Contracts\BrandContract;
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BrandController extends BaseController
+{
+    /**
+     * @var BrandContract
+     */
+    protected $brandRepository;
+
+    /**
+     * CategoryController constructor.
+     * @param BrandContract $brandRepository
+     */
+    public function __construct(BrandContract $brandRepository)
+    {
+        $this->brandRepository = $brandRepository;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $brands = $this->brandRepository->listBrands();
+
+        $this->setPageTitle('Brands', 'List of all brands');
+        return view('admin.brands.index', compact('brands'));
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        $this->setPageTitle('Brands', 'Create Brand');
+        return view('admin.brands.create');
+    }
+}
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -54,5 +54,11 @@
                 <span class="app-menu__label">Categories</span>
             </a>
         </li>
+        <li>
+            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.brands.index' ? 'active' : '' }}" href="{{ route('admin.brands.index') }}">
+                <i class="app-menu__icon fa fa-briefcase"></i>
+                <span class="app-menu__label">Brands</span>
+            </a>
+        </li>
     </ul>
 </aside>
