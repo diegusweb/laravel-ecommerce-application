bd4717b62b44d28a1166ecf22deea34287e449fb
Diego Rueda <diego.rueda2@gmail.com>
2019-09-21 17:48:31 -0400

    new tasks

--- app/Contracts/OrderContract.php
+++ app/Contracts/OrderContract.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: diego
- * Date: 21-09-19
- * Time: 03:14 PM
- */
+
+namespace App\Contracts;
+
+interface OrderContract
+{
+    public function storeOrderDetails($params);
+}
