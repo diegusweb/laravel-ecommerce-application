95eee113a4ff9faf365bdec03f5779dcf96f793a
Diego Rueda <diego.rueda2@gmail.com>
2019-09-21 17:41:02 -0400

    Shopping cart

---
+++ app/Repositories/OrderRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: diego
+ * Date: 21-09-19
+ * Time: 03:15 PM
+ */
+
+namespace App\Repositories;
+
+
+class OrderRepository
+{
+
+}
---
+++ resources/views/site/pages/checkout.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: diego
+ * Date: 21-09-19
+ * Time: 03:18 PM
+ */
